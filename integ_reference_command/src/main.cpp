92e9fb6c7f56330207a9b8b27bda8b7b597f4455
Zhang Sebastien <zhangsebastien1902@gmail.com>
2022-02-23 15:47:53 +0100

    integ_reference_command

--- integ_reference_command/src/main.cpp
+++ integ_reference_command/src/main.cpp
@@ -19,70 +19,66 @@
 #include <calc.h>
 
 using namespace std;
-
+// Initializing the data we need.
 vpColVector q(2);
 vpFeaturePoint s;
-
-// Modify parameters here
-double lambda = 50;
-double l1 = 0.8;
-double l2 = 0.6;
-double offset = 0.1;
-double f = 0.01;
-
 void TargetPointMessageCallback(const geometry_msgs::Pose2D message_s)
 {
-	//this function only sets the received message message_s to the vpFeaturePoint format
-	s.set_x(message_s.x);
-	s.set_y(message_s.y);
+    //this function only sets the received message message_s to the vpFeaturePoint format
+    // The frame of the camera on Gazebo is different from the frame we used, so we used a rotation along y of pi/2.
+    // Don't forget to send the correct velocities value to the publisher
+    s.set_x(-message_s.y);
+    s.set_y(message_s.x);
+    s.set_Z(1);             // is set to 1 for the current problem only.
 }
 
 void JointStatesMessageCallback(const sensor_msgs::JointState message_s)
 {
-	//this function only sets the received message message_s to the vpFeaturePoint format
-	q[0] = message_s.position[0];
-	q[1] = message_s.position[1];
+    //this function only sets the received message message_s to the vpFeaturePoint format
+    q[0] = message_s.position[0];
+    q[1] = message_s.position[1];
 }
-
 int main(int argc, char **argv)
 {
+    // Parameters (modify here if needed) NEED TO CHANGE THEM SO WE GET THEM THROUGH ROS
+
+    double lambda = 50;
+    double l1 = 0.8;
+    double l2 = 0.6;
+    double offset = 0.1;
+    double focal = 0.01;
+
+    // ROS part    featurePointPositionSub.
 	ros::init(argc, argv, "reference_command_node");
+    ros::NodeHandle node;
+    ros::Publisher velocityPub = node.advertise<sensor_msgs::JointState>("desired_joint_v", 1000);
 
-	s.set_Z(1);
-
-	ros::NodeHandle n;
-
-	ros::Publisher velocity_pub = n.advertise<sensor_msgs::JointState>("desired_joint_v", 1000);
-
-	ros::Subscriber cible_position_sub = n.subscribe("target_position", 1000, TargetPointMessageCallback);
-	ros::Subscriber joint_states_sub = n.subscribe("joint_states", 1000, JointStatesMessageCallback);
+    ros::Subscriber featurePointPositionSub = node.subscribe("target_position", 1000, TargetPointMessageCallback);
+    ros::Subscriber jointStatesSub = node.subscribe("joint_states", 1000, JointStatesMessageCallback);
 
 	tf::TransformListener listener;
-
 	ros::Rate loop_rate(10);
 
 	while (ros::ok())
     {
 		auto L = s.interaction(); //L is the interaction matrix
-
 		sensor_msgs::JointState targetJointState; //this is what we will publish
+
         vpVelocityTwistMatrix RotCam = GetRotCam();
         vpVelocityTwistMatrix W = GetW(offset);
         vpVelocityTwistMatrix R = GetR(q[0],q[1]);
 
-		//////////////////////////////////////////////////////////////////////
-		// here we have our computations to get the right values for qPoint //
-		//////////////////////////////////////////////////////////////////////
-		vpMatrix sVector(s.get_s());
+        // Computing what we need to get the joints velocity.
+//		vpMatrix sVector(s.get_s());
+//        auto sPoint = -lambda * sVector;
 
-        auto sPoint = -lambda * RotCam * sVector;
-		auto J = GetJac(q[0], q[1], l1, l2);
+        auto J = GetJac(q[0], q[1], l1, l2);
         auto Js = L * W * R * J;
-        auto qPoint = Js.pseudoInverse() * sPoint;
+        auto qPoint = -Js.pseudoInverse() * lambda * s.get_s();
 
 
         //Deplacement
-        std::pair<double, double> deplacement = Deplacement(l1,l2,q[0],q[1],f,sVector[0],sVector[1]);
+        std::pair<double, double> deplacement = Deplacement(l1,l2,q[0],q[1],focal);//sVector[0],sVector[1]);
 
         // MGI
         std::pair<double, double> angles = MGI(deplacement.first, deplacement.second, l1, l2);
@@ -94,7 +90,7 @@ int main(int argc, char **argv)
 		targetJointState.velocity.push_back(qPoint[1][0]);
         targetJointState.position.push_back(angles.first);
         targetJointState.position.push_back(angles.second);
-		velocity_pub.publish(targetJointState);
+        velocityPub.publish(targetJointState);
 
 		cout << "target speed: " << qPoint << endl;
 
