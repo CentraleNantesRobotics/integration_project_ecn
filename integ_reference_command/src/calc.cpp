91a10faef37a0d0ecac6ab0b0446698e15ddbbf9
Zhang Sebastien <zhangsebastien1902@gmail.com>
2022-03-02 17:48:51 +0100

    ViSP part ended

--- integ_reference_command/src/calc.cpp
+++ integ_reference_command/src/calc.cpp
@@ -14,8 +14,6 @@ vpVelocityTwistMatrix GetW(){
     return vpVelocityTwistMatrix(RotCam3x3);}
 vpVelocityTwistMatrix GetR(double q1,double q2){return vpVelocityTwistMatrix(0,0,0,-(q1+q2),0,0);}
 
-vpVelocityTwistMatrix GetRotCamToGazebo(vpRotationMatrix &R){return vpVelocityTwistMatrix(R);}  // Le repère cam dans gazebo est différent du repère caméra usuel.
-
 vpMatrix GetJac(double q1, double q2, double l1, double l2)
 {
 	vpMatrix Jac(6, 2);
@@ -34,13 +32,13 @@ vpMatrix GetJac(double q1, double q2, double l1, double l2)
 
 	return Jac;
 }
-std::pair<double, double> MGD(double l1, double l2, double q1, double q2){return {l1*cos(q1)+l2*cos(q1+q2), l1*sin(q1)+l2*sin(q1+q2)};}
+std::pair<double, double> MGD(double l1, double l2, double q1, double q2){return  std::pair<double, double> {l1*cos(q1)+l2*cos(q1+q2), l1*sin(q1)+l2*sin(q1+q2)};}
 
 std::pair<double, double> MGI(double Y, double Z, double l1, double l2)
 {
     double q2d = acos((Y*Y+ Z*Z-l1*l1-l2*l2)/(2*l1*l2));
     double q1d = atan2(Z, Y)-(l1+l2*cos(q2d))/(pow(Y*Y+Z*Z,0.5));
-    return {q1d, q2d};
+    return  std::pair<double, double> {q1d, q2d};
 }
 
 std::pair<double, double> Deplacement(double l1, double l2, double q1, double q2,double f, const double x, const double y)
@@ -49,7 +47,7 @@ std::pair<double, double> Deplacement(double l1, double l2, double q1, double q2
     double Y = Pc.first - cos(q1+q2)*(x*(1+1/f))-sin(q1+q2)*(y*(1+1/f));
     double Z = Pc.second - cos(q1+q2)*(y*(1+1/f))-sin(q1+q2)*(x*(1+1/f));
 
-    return {Y, Z};
+    return std::pair<double, double> {Y, Z};
 }
 
 using namespace std;
--- integ_reference_command/src/main.cpp
+++ integ_reference_command/src/main.cpp
@@ -33,29 +33,31 @@ void TargetPointMessageCallback(const geometry_msgs::Pose2D message_s)
     s.set_Z(1);             // is set to 1 for the current problem only.
 }
 
-
 void JointStatesMessageCallback(const sensor_msgs::JointState message_s)
 {
     //this function only sets the received message message_s to the vpFeaturePoint format
     q[0] = message_s.position[0];
     q[1] = message_s.position[1];
 }
+
 int main(int argc, char **argv)
 {
     // Parameters (modify here if needed) NEED TO CHANGE THEM SO WE GET THEM THROUGH ROS
-
+    cout << "TEST LIGNE DE CODE" << endl;
     double lambda = 50;
     double l1 = 0.8;
     double l2 = 0.6;
     double focal = 0.01;
 
     // ROS part    featurePointPositionSub.
-	ros::init(argc, argv, "reference_command_node");
+    ros::init(argc, argv, "integ_reference_command");
     ros::NodeHandle node;
     ros::Publisher velocityPub = node.advertise<sensor_msgs::JointState>("camera_trajectory", 1000);
 
-    ros::Subscriber featurePointPositionSub = node.subscribe("target_position", 1000, TargetPointMessageCallback);
-    ros::Subscriber jointStatesSub = node.subscribe("joint_states", 1000, JointStatesMessageCallback);
+
+
+    ros::Subscriber featurePointPositionSub = node.subscribe("masscenter", 1000, TargetPointMessageCallback);
+    ros::Subscriber jointStatesSub = node.subscribe("state", 1000, JointStatesMessageCallback);
 
 	tf::TransformListener listener;
 	ros::Rate loop_rate(10);
@@ -63,39 +65,42 @@ int main(int argc, char **argv)
 
 	while (ros::ok())
     {
-		auto L = s.interaction(); //L is the interaction matrix
+        vpMatrix L = s.interaction(); //L is the interaction matrix
 		sensor_msgs::JointState targetJointState; //this is what we will publish
 
-        //vpVelocityTwistMatrix RotCam6x6 = GetRotCamToGazebo(RotCam3x3);
         vpVelocityTwistMatrix W = GetW();
         vpVelocityTwistMatrix R = GetR(q[0],q[1]);
 
         // Computing what we need to get the joints velocity.
 
         auto sPoint = - lambda * s.get_s();
-        auto J = GetJac(q[0], q[1], l1, l2);
-        auto Jc = W * R * J;
-        auto Js = L * Jc;
+        vpMatrix J = GetJac(q[0], q[1], l1, l2);
+        vpMatrix Jc = W * R * J;
+        vpMatrix Js = L * Jc;
         auto qPoint = Js.pseudoInverse() * sPoint;
 
-        //Displacement to get to the center of the target.
+       //Displacement to get to the center of the target.
         std::pair<double, double> deplacement = Deplacement(l1,l2,q[0],q[1],focal, s.get_x(), s.get_y());
         // MGI to get the value of the angles.
         std::pair<double, double> angles = MGI(deplacement.first, deplacement.second, l1, l2);
 
-        // Valeurs à renvoyer
-        targetJointState.name.push_back("moteur1");
-        targetJointState.name.push_back("moteur2");
+       // Valeurs à renvoyer
+        targetJointState.name.push_back("joint 1");
+        targetJointState.name.push_back("joint 2");
         targetJointState.velocity.push_back(qPoint[0]);
         targetJointState.velocity.push_back(qPoint[1]);
         targetJointState.position.push_back(angles.first);
         targetJointState.position.push_back(angles.second);
         velocityPub.publish(targetJointState);
 
-		cout << "target speed: " << qPoint << endl;
+        cout << "target speed: " << endl;
+        cout << qPoint << endl;
+        cout << "target position: " << endl;
+        cout << angles.first << "  "<< angles.second << endl;
 
-		ros::spinOnce();
-		loop_rate.sleep();
+
+        ros::spinOnce();
+        loop_rate.sleep();
 	}
 
 	return 0;
