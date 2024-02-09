9236f683bf90ab72411c210b7bc2da1019e5f487
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-02-09 14:45:36 +0100

    add position and velocity control for Gazebo

--- integ_description/launch/state_publisher_launch.py
+++ integ_description/launch/state_publisher_launch.py
@@ -5,9 +5,11 @@ def generate_launch_description():
     
     sl = SimpleLauncher(use_sim_time = False)
     
+    sl.declare_arg('mode', 'position')
     sl.declare_arg('jsp', False)
 
-    sl.robot_state_publisher('integ_description', 'scara_sw.urdf')
+    sl.robot_state_publisher('integ_description', 'scara_sw.xacro',
+                             xacro_args=sl.arg_map('mode'))
 
     sl.rviz(sl.find('integ_description', 'config.rviz'))
 
--- integ_description/launch/upload_launch.py
+++ integ_description/launch/upload_launch.py
@@ -1,20 +1,45 @@
 from simple_launch import SimpleLauncher, GazeboBridge
 
+sl = SimpleLauncher(use_sim_time = True)
+sl.declare_arg('mode', 'position')
+sl.declare_arg('sliders', False)    # for manual control
 
-def generate_launch_description():
 
-    sl = SimpleLauncher(use_sim_time = True)
+def launch_setup():
+
+    mode = sl.arg('mode')
+
     sl.include('integ_description', 'state_publisher_launch.py',
-               launch_arguments = {'use_sim_time': True})
+               launch_arguments = {'use_sim_time': True, 'mode': mode})
                     
     # URDF spawner to Gazebo, defaults to relative robot_description topic
     sl.spawn_gz_model('scara')
+
+    # sliders
+    if sl.arg('sliders'):
+        sl.node('slider_publisher', arguments = [sl.find('integ_description', f'{mode}_manual.yaml')])
             
     # ROS-Gz bridges
     bridges = []
     gz_js_topic = GazeboBridge.model_prefix('scara') + '/joint_state'
     bridges.append(GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros))
 
+    for joint in ('joint_1', 'joint_2'):
+
+        if mode == 'position':
+            bridges.append(GazeboBridge(f'/model/scara/joint/{joint}/0/cmd_pos',
+                                        f'{joint}_cmd_pos',
+                                        'std_msgs/Float64',
+                                        GazeboBridge.ros2gz))
+        elif mode == 'velocity':
+            bridges.append(GazeboBridge(f'{joint}_cmd_vel',
+                                        f'{joint}_cmd_vel',
+                                        'std_msgs/Float64',
+                                        GazeboBridge.ros2gz))
+
     sl.create_gz_bridge(bridges)
 
     return sl.launch_description()
+
+
+generate_launch_description = sl.launch_description(launch_setup)
