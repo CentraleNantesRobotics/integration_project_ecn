9ed4e768648f6581d9860828b446f7b7524aa3d3
Thibault LEBLANC <tl.leblanc@outlook.fr>
2024-03-06 09:15:23 +0100

    .

--- build-integ_command-Imported_Kit-Debug/CMakeFiles/integ_command.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for integ_command.
-# This may be replaced when dependencies are built.
--- build-integ_command-Imported_Kit-Debug/CMakeFiles/integ_command_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/integ_command_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/integ_command_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
