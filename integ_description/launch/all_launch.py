d690db8e47dea0309f0734330f61a72cbdac0e8c
T-Lemmel <lemmeltom@gmail.com>
2024-03-08 11:40:38 +0100

    super launch with args

--- integ_description/launch/all_launch.py
+++ integ_description/launch/all_launch.py
@@ -9,6 +9,7 @@ def generate_launch_description():
     sl.declare_arg('vel', 1.)
     sl.declare_arg('lambda', 1.)  # visual servoing proportional gain
     sl.declare_arg('use_vs', False)
+    sl.declare_arg('debug', False)
 
     sl.include('integ_description', 'upload_launch.py',
                launch_arguments={'use_sim_time': True, 'mode': sl.arg('mode'), 'sliders': sl.arg('sliders')})
@@ -17,7 +18,12 @@ def generate_launch_description():
                launch_arguments={'use_sim_time': True, 'vel': sl.arg('vel')})
 
     with sl.group(if_arg = 'use_vs') :
-        sl.node(package='visual_servoing', executable='FeatureDetector', output='screen')
-        sl.node(package='visual_servoing', executable='tracker', output='screen', parameters=[{'lambda': sl.arg('lambda')}])
+        with sl.group(if_arg = 'debug'):
+            sl.node(package='visual_servoing', executable='FeatureDetector', output='screen')
+            sl.node(package='visual_servoing', executable='tracker', output='screen', parameters=[{'lambda': sl.arg('lambda')}])
+        with sl.group(unless_arg = 'debug'):
+            with sl.group(unless_arg='debug'):
+                sl.node(package='visual_servoing', executable='FeatureDetector', output='screen')
+                sl.node(package='visual_servoing', executable='tracker', output='screen', parameters=[{'lambda': sl.arg('lambda')}])
         
     return sl.launch_description()
