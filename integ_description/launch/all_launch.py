6a635e1ed9f8f77a879cf31e096bf28b97ec3b54
T-Lemmel <lemmeltom@gmail.com>
2024-03-08 11:20:54 +0100

    super launch with args

--- integ_description/launch/all_launch.py
+++ integ_description/launch/all_launch.py
@@ -1,10 +1,23 @@
 from simple_launch import SimpleLauncher
 
-
 def generate_launch_description():
+    
+    sl = SimpleLauncher()
+    
+    sl.declare_arg('mode', 'velocity')
+    sl.declare_arg('sliders', False)  # for manual control
+    sl.declare_arg('vel', 1.)
+    sl.declare_arg('lambda', 1.)  # visual servoing proportional gain
+    sl.declare_arg('use_vs', False)
 
-    sl = SimpleLauncher(use_sim_time = True)
-    for prefix in ('target', 'upload'):
-        sl.include('integ_description', f'{prefix}_launch.py')
+    sl.include('integ_description', 'upload_launch.py',
+               launch_arguments={'use_sim_time': True, 'mode': sl.arg('mode'), 'sliders': sl.arg('sliders')})
 
+    sl.include('integ_description', 'target_launch.py',
+               launch_arguments={'use_sim_time': True, 'vel': sl.arg('vel')})
+
+    with sl.group(if_arg = 'use_vs') :
+        sl.node(package='visual_servoing', executable='FeatureDetector', output='screen')
+        sl.node(package='visual_servoing', executable='tracker', output='screen', parameters=[{'lambda': sl.arg('lambda')}])
+        
     return sl.launch_description()
