96e55e54ca56d7102b5fdd60ab4ba125b4ac6115
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-03-08 11:04:11 +0100

    forward target tf

--- integ_description/launch/target_launch.py
+++ integ_description/launch/target_launch.py
@@ -1,4 +1,4 @@
-from simple_launch import SimpleLauncher
+from simple_launch import SimpleLauncher, GazeboBridge
 
 
 def generate_launch_description():
@@ -11,5 +11,8 @@ def generate_launch_description():
         sl.robot_state_publisher('integ_description', 'target.xacro', xacro_args=sl.arg_map('vel'))
 
         sl.spawn_gz_model('target')
+
+        gz_js_topic = GazeboBridge.model_prefix('target') + '/joint_state'
+        sl.create_gz_bridge(GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros))
 
     return sl.launch_description()
