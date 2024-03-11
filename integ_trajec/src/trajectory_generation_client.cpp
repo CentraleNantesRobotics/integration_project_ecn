e81537f88f9b1bbe53615f43e5065511437e82d9
Thibault LEBLANC <tl.leblanc@outlook.fr>
2024-03-11 12:53:03 +0100

    integ_trajec/

--- integ_trajec/src/trajectory_generation_client.cpp
+++ integ_trajec/src/trajectory_generation_client.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "integ_msgs/srv/srv_trajec.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+#include "std_msgs/msg/float64.hpp"
 
 
 using namespace std::chrono_literals;
@@ -19,23 +20,55 @@ class TrajectoryClient : public rclcpp::Node
     {
         client = this->create_client<integ_msgs::srv::SrvTrajec>("srvtrajec");
 
-        publisher_waypoint = this->create_publisher<sensor_msgs::msg::JointState>("desired_joint_state",10);
+        publisher_waypoint = this->create_publisher<sensor_msgs::msg::JointState>("/scara/desired_joint_states",10);
 
-        subscriber_initial_pose = this->create_subscription<sensor_msgs::msg::JointState>("joint_state", 10, std::bind(&TrajectoryClient::callback_initial_pose, this, _1));
+        subscriber_initial_pose = this->create_subscription<sensor_msgs::msg::JointState>("/scara/joint_states", 10, std::bind(&TrajectoryClient::callback_initial_pose, this, _1));
 
-        subscriber_goal_pose = this->create_subscription<sensor_msgs::msg::JointState>("desired_pose", 10, std::bind(&TrajectoryClient::callback_goal_pose, this, _1));
+        q1_ = create_subscription<std_msgs::msg::Float64>(
+                    "/q1_chosen",
+                    10,
+                    [this](const std_msgs::msg::Float64::SharedPtr msg) {
+                    q1Callback(msg);
+        });
+
+        q2_ = create_subscription<std_msgs::msg::Float64>(
+                    "/q2_chosen",
+                    10,
+                    [this](const std_msgs::msg::Float64::SharedPtr msg) {
+                    q2Callback(msg);
+                    });
     }
 
   private:
+
+    double q1;
+    double q2;
+
+    //Callback pour le Kp, utile dans le cas d'une optimisation par sliderpublisher par exemple ou pour un calcul externe du Kp
+    void q1Callback(std_msgs::msg::Float64::SharedPtr k){
+        q1=k->data;
+        callback_goal_pose();
+
+    }
+
+    //Callback pour le Kd, utile dans le cas d'une optimisation par sliderpublisher par exemple ou pour un calcul externe du Kd
+    void q2Callback(std_msgs::msg::Float64::SharedPtr k){
+        q2=k->data;
+        callback_goal_pose();
+
+    }
 
     void callback_initial_pose(const sensor_msgs::msg::JointState::SharedPtr initial_pose)
     {
         request->initial_pose = *initial_pose;
     }
 
-    void callback_goal_pose(const sensor_msgs::msg::JointState::SharedPtr goal_pose)
+    void callback_goal_pose()
     {
-        request->goal_pose = *goal_pose;
+
+        request->goal_pose.position.push_back(q1);
+        request->goal_pose.position.push_back(q2);
+
 
         auto result = client->async_send_request(request);
         response->trajectory = result.get()->trajectory;
@@ -73,6 +106,8 @@ class TrajectoryClient : public rclcpp::Node
 
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscriber_initial_pose;
     rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscriber_goal_pose;
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr q1_;
+    rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr q2_;
 
 };
 
