65dba6ee177b9ba84b952d2bd7d4f2c2968a0333
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2024-03-14 11:09:02 +0100

    boule2noel

--- integ_description/launch/target_launch.py
+++ integ_description/launch/target_launch.py
@@ -1,18 +1,30 @@
 from simple_launch import SimpleLauncher, GazeboBridge
 
+sl = SimpleLauncher(use_sim_time = True)
+sl.declare_arg('vel', 1.)
+sl.declare_arg('type', 'target')
 
-def generate_launch_description():
 
-    sl = SimpleLauncher(use_sim_time = True)
-    sl.declare_arg('vel', 1.)
+def launch_setup():
 
-    with sl.group(ns = 'target'):
+    target = sl.arg('type')
+    vel = sl.arg('vel') if target == 'target' else 0.
 
-        sl.robot_state_publisher('integ_description', 'target.xacro', xacro_args=sl.arg_map('vel'))
+    with sl.group(ns = target):
 
-        sl.spawn_gz_model('target')
+        sl.robot_state_publisher('integ_description', f'{target}.xacro', xacro_args={'vel': vel})
 
-        gz_js_topic = GazeboBridge.model_prefix('target') + '/joint_state'
+        sl.spawn_gz_model(target)
+
+        gz_js_topic = GazeboBridge.model_prefix(target) + '/joint_state'
         sl.create_gz_bridge(GazeboBridge(gz_js_topic, 'joint_states', 'sensor_msgs/JointState', GazeboBridge.gz2ros))
-
     return sl.launch_description()
+
+
+generate_launch_description = sl.launch_description(launch_setup)
+
+
+
+
+
+
